41c99afc4258e2d6cb244a4ca226a3b88481af71
steven <wenhuachen.cn@gmail.com>
2024-01-06 14:35:23 +1030

    update

--- words_count.py
+++ words_count.py
@@ -3,20 +3,18 @@
 # Website: https://wenhua-chen.github.io/
 # Github: https://github.com/wenhua-chen
 # Date: 2023-12-28 11:14:52
-# LastEditTime: 2024-01-06 14:24:51
+# LastEditTime: 2024-01-06 14:34:23
 # Description: 统计单词量
 
 import glob
 
-# path = '我的单词本'
-path = '新增单词本'
+for path in ['我的单词本', '新增单词本']:
+    files = glob.glob(f'{path}/**/*.txt', recursive=True)
 
-files = glob.glob(f'{path}/**/*.txt', recursive=True)
+    mywords = set()
+    for file in files:
+        with open(file,'r') as f:
+            words = set([word.strip().lower() for word in f.readlines() if len(word.strip())>0])
+        mywords = mywords.union(words)
 
-mywords = set()
-for file in files:
-    with open(file,'r') as f:
-        words = set([word.strip().lower() for word in f.readlines() if len(word.strip())>0])
-    mywords = mywords.union(words)
-
-print(f'"{path}"的单词量: {len(mywords)}')
+    print(f'"{path}"的单词量: {len(mywords)}')
