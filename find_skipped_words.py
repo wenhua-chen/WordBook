590e6e91ec561a30e52f7d6f18c5f3b67a750327
Steven <wenhuachen.cv@gmail.com>
2024-05-28 09:08:00 +0800

    未掌握->基本掌握: pte, taylor, the idea

--- find_skipped_words.py
+++ find_skipped_words.py
@@ -1,8 +1,8 @@
 # 找到在添加到多墨时, 漏掉的单词
 
 if __name__ == '__main__':
-    in_file1 = '我的单词本/完全掌握/Friends.S01E01-05.txt' # 原有单词表
-    in_file2 = 'friends-全部(137) .txt' # 导入多墨后的单词表
+    in_file1 = '我的单词本/未掌握/Under.The.Tuscan.Sun.2003.1080p.BluRay.H264.AAC.txt' # 原有单词表
+    in_file2 = 'under-全部(301).txt' # 导入多墨后的单词表
 
     with open(in_file1,'r') as f:
         file1_words = set([word.strip().lower() for word in f.readlines() if len(word.strip())>0])
