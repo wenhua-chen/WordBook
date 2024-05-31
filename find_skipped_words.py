a703a0bd69c384daa825fe33c506836e7221b4ef
Steven <wenhuachen.cv@gmail.com>
2024-05-31 10:08:06 +0800

    未掌握完成第一遍过滤，接下来第二遍过基本掌握

--- find_skipped_words.py
+++ find_skipped_words.py
@@ -1,8 +1,8 @@
 # 找到在添加到多墨时, 漏掉的单词
 
 if __name__ == '__main__':
-    in_file1 = '我的单词本/未掌握/Under.The.Tuscan.Sun.2003.1080p.BluRay.H264.AAC.txt' # 原有单词表
-    in_file2 = 'under-全部(301).txt' # 导入多墨后的单词表
+    in_file1 = 'Pulp.Fiction.1994.720p.BluRay.H264.AAC.txt' # 原有单词表
+    in_file2 = 'pulp+taylor-全部(198).txt' # 导入多墨后的单词表
 
     with open(in_file1,'r') as f:
         file1_words = set([word.strip().lower() for word in f.readlines() if len(word.strip())>0])
@@ -13,5 +13,3 @@ if __name__ == '__main__':
     with open(in_file2, 'w') as f:
         for word in words:
             f.write(f'{word}\n')
-
-
